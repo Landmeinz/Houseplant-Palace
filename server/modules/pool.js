c6746262ca306162e67fb1f6b4cff05976e00ca6
Eric Meinzer <emeinzer7@gmail.com>
2021-12-27 14:43:10 -0600

    setup for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -11,25 +11,28 @@ if (process.env.DATABASE_URL) {
   const auth = params.auth.split(':');
 
   config = {
-    user:     auth[0],
+    user: auth[0],
     password: auth[1],
-    host:     params.hostname,
-    port:     params.port,
+    host: params.hostname,
+    port: params.port,
     database: params.pathname.split('/')[1],
-    ssl:      { rejectUnauthorized: false },
-    max:      10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
-} else {
-  config = {
-    user: process.env.PG_USER               || null, //env var: PGUSER
-    password: process.env.DATABASE_SECRET   || null, //env var: PGPASSWORD
-    host: process.env.DATABASE_SERVER       || 'localhost', // Server hosting the postgres database
-    port: process.env.DATABASE_PORT         || 5432, //env var: PGPORT
-    database: process.env.DATABASE_NAME     || 'houseplant_palace', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+    ssl: { rejectUnauthorized: false },
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-};
+  };
+
+} else {
+
+  config = {
+    user: process.env.PG_USER             || null, //env var: PGUSER
+    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+    host: process.env.DATABASE_SERVER     || 'localhost', // Server hosting the postgres database
+    port: process.env.DATABASE_PORT       || 5432, //env var: PGPORT
+    database: process.env.DATABASE_NAME   || 'houseplant_palace', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+
 }
 
 // this creates the pool that will be shared by all other modules
